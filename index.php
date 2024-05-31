e4410ded63cba28aba5891ff6d4a9d34a2d44d64
rendidwi2604 <144294907+rendidwi2604@users.noreply.github.com>
2024-05-31 23:56:45 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+
+<?php
+$title="Dashboard";
+$h1="WELCOME";
+$body1="Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa magni odio totam provident modi, unde enim eius fugiat, repellat recusandae voluptatum magnam nesciunt architecto porro dolor suscipit repellendus rerum. Itaque aperiam, quo ullam minus praesentium eveniet vitae vel laborum laboriosam at quae impedit nostrum iste neque, veniam nisi ab corrupti?";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title ?></title>
+</head>
+
+<body>
+    <?php echo $h1 ?>
+    <p>
+    <?php echo $body1 ?>
+</p>
+<style>
+    h1{
+        font-size :100px:
+    }
+</style>
+    
+</body>
+</html>
